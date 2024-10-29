fcee8787070437c3b4d3b19ced710de6ddcc0edb
Cezar - Escandor <162781412+yachiruuuuu@users.noreply.github.com>
2024-10-29 14:35:05 +0800

    sqlite

--- MiniFB-main/MiniFB/settings.py
+++ MiniFB-main/MiniFB/settings.py
@@ -80,14 +80,11 @@ WSGI_APPLICATION = 'MiniFB.wsgi.application'
 
 DATABASES = {
     'default': {
-       'ENGINE': 'django.db.backends.mysql',
-       'NAME' : 'minifb',
-       'USERNAME' : 'arnoldm',
-       'PASSWORD' : '12345',
-       'HOST' : 'localhost',
-       'PORT' : '3306'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
     }
 }
+
 
 
 # Password validation
